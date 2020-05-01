add002b8d13972f3d45397d3a569c2d701e1b9d4
CorreiaAlberto <alberto_cpv@hotmail.com>
2020-05-01 13:52:18 +0200

    refactor

--- javascript/trivia.js
+++ javascript/trivia.js
@@ -1,6 +1,6 @@
 //Preguntas
 const preguntas = [{
-        pregunta: "¿Que cocktail popularizó la pelicula El gran Lebowsky",
+        pregunta: "¿Que cocktail popularizó la pelicula El gran Lebowsky?",
         a: "Cosmopolitan",
         b: "Old fashioned",
         c: "Mojito",
@@ -24,18 +24,20 @@ const preguntas = [{
         img: "../imgs/el padrino.jpg"
     }
 ]
-// Variables de acceso
-let preguntaActual = document.getElementById("pregunta")
-let heroImg = document.getElementById("hero")
-let player1Score = document.getElementById("score1")
-let player2Score = document.getElementById("score2")
-let OpcionA = document.getElementById("a")
-let OpcionB = document.getElementById("b")
-let OpcionC = document.getElementById("c")
-let OpcionD = document.getElementById("d")
-
-
-
+// Aplico un shuffle para poder pasar orden de preguntas distinto cada vez
+function shuffle(arr) {
+    var i,
+        j,
+        temp;
+    for (i = arr.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    return arr;
+};
+let preguntasMezcladas = shuffle(preguntas)
 
 //Constructor Player
 class Player {
@@ -45,33 +47,88 @@ class Player {
     }
 }
 
-//Constructor de Ronda
-class Ronda {
-    constructor(pregunta) {
-        this.preguntas = preguntas
-        pregunta = this.seleccionPregunta
+//Constructor de partida
+class Partida {
+    constructor() {
+        this.preguntas = preguntasMezcladas
+        this.preguntaActual = preguntas[0].pregunta
+        this.Indice = 0
         this.preguntasRealizadas = []
-
-
     }
-    //Seleccion pregunta
-    seleccionPregunta() {
-        return this.preguntas[Math.floor(Math.random() * this.preguntas.length)]
-    }
-    //Comprobamos si es la primera vez que hacemos la pregunta
-    primeraVez() {
-        if (this.pregunta.indexOf(this.preguntasRealizadas) == -1) {
-            return true
-        } else {
-            this.seleccionPregunta()
-        }
-    }
-    //Evaluamos si la respuesta es correcta y añadimos score
+    //Comprobar respuesta ganadora
     respuestaCorrecta(respuestaUsuario) {
-        if (respuestaUsuario === this.pregunta.correcta) {
-            Player.score = Player.score + 5
+        if (respuestaUsuario == preguntas[this.indice].correcta) {
+            return true
         }
+    }
+    //Actualizar Score
+    actualizarScore() {
+        if (this.respuestaCorrecta) {
+            score = score + 5
+        }
+    }
 
+    //Metodo para siguiente pregunta
+    siguientePregunta() {
+        indice++
+        this.preguntaActual = this.preguntas[this.indice]
+    }
 
+}
+class Board {
+    constructor() {
+        this.splash = document.getElementById("splash")
+        this.preguntaActual = document.getElementById("pregunta")
+        this.heroImg = document.getElementById("hero")
+        this.player1Score = document.getElementById("score1")
+        this.player2Score = document.getElementById("score2")
+        this.opcionA = document.getElementById("a")
+        this.opcionB = document.getElementById("b")
+        this.opcionC = document.getElementById("c")
+        this.opcionD = document.getElementById("d")
     }
 }
+let board = new Board()
+let player1 = new Player("alberto")
+
+
+//Cambio Splash
+document.getElementById("inicio").onclick = (evento) => {
+    //esconder splash
+    document.getElementById("splash").style.display = "none"
+    //Mostrar pregunta
+    document.getElementById("preguntas").style.display = "inline"
+    //Mostrar player 1
+    document.getElementById("player1").style.display = 'inline'
+    // document.getElementById("player").innerHTML =
+    //Mostrar 1 pregunta
+    document.getElementById("pregunta").innerHTML = preguntas[0].pregunta
+    //Mostrar 1 imagen
+    document.getElementById("hero").innerHTML = preguntas[0].img
+    //Mostrar opciones respuesta
+    document.getElementById("a").innerHTML = preguntas[0].a
+    document.getElementById("b").innerHTML = preguntas[0].b
+    document.getElementById("c").innerHTML = preguntas[0].c
+    document.getElementById("d").innerHTML = preguntas[0].correcta
+    //inicio partida
+    new Partida()
+
+
+}
+
+
+/* clase partida ()
+constructor ()
+
+
+preguntas
+preguntaSeleccionada = 1
+indice = 0
+function siguientePregunta() {
+    indice ++
+    this.preguntaSeleccionada= this.preguntas[this.indice]
+}
+
+const partidaNueva = new partida () 
+partidaNueva.siguientePregunta() 
+partidaNueva.partidaSeleccionada*/
