2fc1bb39bf3ed3611fac56ab4b0bd135ec0c3dcd
CorreiaAlberto <alberto_cpv@hotmail.com>
2020-05-06 19:44:51 +0200

    refactor

--- README.md
+++ README.md
@@ -8,35 +8,33 @@ su puntuacion con otros usuarios para saber quien es el que mas sabe de cocktail
 ## Estados y reglas:
 
 - El usuario empieza con un score de 0 puntos
-- Las respuestas pueden tener distinto grado de aceptabilidad, dando distinto numero de puntos a una respuesta
-  parcialmente correcta y a una totalmente correcta.
-  -El usuario con mayor puntuación gana la partida.
+- Cada respuesta correcta añadirá 5 puntos a tu Score
+-
 
 ## MVP:
 
 - El usuario respondera preguntas
 - El usuario obtendrá puntos por preguntas acertadas, que almacenará en su "score"
-- El juego acabará en "x" preguntas.
+- El juego acabará en 10 preguntas.
 
 ## Back-log:
 
 - El usuario podrá introducir su nombre
-- Multijugador
-- El usuario podrá comparar su score al adversario y 1 resultará vencedor
 - El usuario tendrá un tiempo limitado para responder a las preguntas
-- El usuario podrá elegir idioma (Español, Ingles,Italiano)
 - Display de imagenes ilustrativas, que refuercen el concepto y sean memorables para el usuario
+- El juego tendra sonidos
 
 Estructura de datos:
 -DOM
 
 - Constructor Clase player (Nombre)
-- Metodo seleccionPregunta () ===> selecciona una pregunta aleatoria almacenada en una variable.
-- Metodo primeraVez () ===> Evalua si es la primera vez que la pregunta es escogida, en caso negativo selecciona otra.
-- Metodo shuffleAnswers () ===> Hace un display ramdon de las opciones de respuesta
-- Metodo respestaCorrecta () ===> valora si la respuesta es correcta y añade puntuacion al score.
-- Metodo gameOver() ===> pasadas "x" rondas, devuelve el score y para el juego.
-- Metodo ganador //para multijugador () ===> Compara el metodo score() de ambos jugadores y retorna el ganador.
+- Constructor Clase partida ()
+- Metodo respuestaCorrecta () ==> Evalua si la respuesta dedl usuario es correcta.
+- Metodo actualizarScore() ==> Añade puntuacion a tu Score
+- Metodo Timer () ==> Da un tiempo para responder las preguntas
+- Metodo gameOver () ==> Evalua si se ha llegadod a la ultima pregunta y muestra el splash de gameOver y la score final
+- Metodo siguientePregunta () ==> Hace el display de todos los elementos neccesarios para el juego y los actualizaz a cada ronda
+- Constructor clase Board ()
 
 ## Links
 
--- javascript/trivia.js
+++ javascript/trivia.js
@@ -1,148 +1,295 @@
 //Preguntas
-const preguntas = [
-  {
+const preguntas = [{
     pregunta: "¿Que cocktail popularizó la pelicula El gran Lebowsky?",
-    respuestas: [
-      { resp: "Cosmopolitan", correcta: false },
-      { resp: "Old fashioned", correcta: false },
-      { resp: "Mojito", correcta: false },
-      { resp: "White russian", correcta: true },
+    respuestas: [{
+        resp: "Cosmopolitan",
+        correcta: false
+      },
+      {
+        resp: "Old fashioned",
+        correcta: false
+      },
+      {
+        resp: "Mojito",
+        correcta: false
+      },
+      {
+        resp: "White russian",
+        correcta: true
+      },
     ],
     img: "./imgs/Ruso-blanco.jpg.",
   },
   {
     pregunta: "¿Cual de los siguientes ingredientes NO lleva un bloody mary?",
-    respuestas: [
-      { resp: "Salsa Worcester", correcta: false },
-      { resp: "Zumo de tomate", correcta: false },
-      { resp: "Tabasco", correcta: false },
-      { resp: "Ginebra", correcta: true },
+    respuestas: [{
+        resp: "Salsa Worcester",
+        correcta: false
+      },
+      {
+        resp: "Zumo de tomate",
+        correcta: false
+      },
+      {
+        resp: "Tabasco",
+        correcta: false
+      },
+      {
+        resp: "Ginebra",
+        correcta: true
+      },
     ],
     img: "./imgs/bloody mary.jpg",
   },
   {
-    pregunta:
-      "¿Que cocktail hizo aparición durante la segunda parte de El padrino?",
-    respuestas: [
-      { resp: "Dry martini", correcta: false },
-      { resp: "Tom Collins", correcta: false },
-      { resp: "Negroni", correcta: false },
-      { resp: "Banana daiquiri", correcta: true },
+    pregunta: "¿Que cocktail hizo aparición durante la segunda parte de El padrino?",
+    respuestas: [{
+        resp: "Dry martini",
+        correcta: false
+      },
+      {
+        resp: "Tom Collins",
+        correcta: false
+      },
+      {
+        resp: "Negroni",
+        correcta: false
+      },
+      {
+        resp: "Banana daiquiri",
+        correcta: true
+      },
     ],
     img: "./imgs/el padrino.jpg",
   },
   {
     pregunta: "¿Cual es el nombre del cocktail favorito de James Bond?",
-    respuestas: [
-      { resp: "Vodka Martini", correcta: false },
-      { resp: "Coco loco", correcta: false },
-      { resp: "Bondtini", correcta: false },
-      { resp: "Vesper Martini", correcta: true },
+    respuestas: [{
+        resp: "Vodka Martini",
+        correcta: false
+      },
+      {
+        resp: "Coco loco",
+        correcta: false
+      },
+      {
+        resp: "Bondtini",
+        correcta: false
+      },
+      {
+        resp: "Vesper Martini",
+        correcta: true
+      },
     ],
     img: "./imgs/james bond.jpg",
   },
   {
-    pregunta:
-      "¿Cual es el nombre del cocktail que vemos preparar en la imagen a Jerry Thomas? ",
-    respuestas: [
-      { resp: "Blue lagoon", correcta: false },
-      { resp: "Manhattan", correcta: false },
-      { resp: "Mint julep", correcta: false },
-      { resp: "Blue blazer", correcta: true },
+    pregunta: "¿Cual es el nombre del cocktail que vemos preparar en la imagen a Jerry Thomas? ",
+    respuestas: [{
+        resp: "Blue lagoon",
+        correcta: false
+      },
+      {
+        resp: "Manhattan",
+        correcta: false
+      },
+      {
+        resp: "Mint julep",
+        correcta: false
+      },
+      {
+        resp: "Blue blazer",
+        correcta: true
+      },
     ],
     img: "./imgs/Jerry-Thomas.jpg",
   },
   {
-    pregunta:
-      "¿Que establecimiento cubano comercializó por primerar vez el popular mojito?",
-    respuestas: [
-      { resp: "La floridita", correcta: false },
-      { resp: "El bribón de la havana", correcta: false },
-      { resp: "La cantina del Malecón", correcta: false },
-      { resp: "La bodedguita de en medio", correcta: true },
+    pregunta: "¿Que establecimiento cubano comercializó por primerar vez el popular mojito?",
+    respuestas: [{
+        resp: "La floridita",
+        correcta: false
+      },
+      {
+        resp: "El bribón de la havana",
+        correcta: false
+      },
+      {
+        resp: "La cantina del Malecón",
+        correcta: false
+      },
+      {
+        resp: "La bodedguita de en medio",
+        correcta: true
+      },
     ],
     img: "./imgs/mojito.jpg",
   },
   {
-    pregunta:
-      "¿Cual es la bebida favorita de Don Draper, protagonista de Mad men?",
-    respuestas: [
-      { resp: "Negroni", correcta: false },
-      { resp: "Long island ice tea", correcta: false },
-      { resp: "Agua con gas", correcta: false },
-      { resp: "Old fashioned", correcta: true },
+    pregunta: "¿Cual es la bebida favorita de Don Draper, protagonista de Mad men?",
+    respuestas: [{
+        resp: "Negroni",
+        correcta: false
+      },
+      {
+        resp: "Long island ice tea",
+        correcta: false
+      },
+      {
+        resp: "Agua con gas",
+        correcta: false
+      },
+      {
+        resp: "Old fashioned",
+        correcta: true
+      },
     ],
     img: "./imgs/madmen.jpg",
   },
   {
     pregunta: "¿Cual es la base alcoholica predominante en la coctelería Tiki?",
-    respuestas: [
-      { resp: "Vodka", correcta: false },
-      { resp: "Whisky", correcta: false },
-      { resp: "Ginebra", correcta: false },
-      { resp: "Ron", correcta: true },
+    respuestas: [{
+        resp: "Vodka",
+        correcta: false
+      },
+      {
+        resp: "Whisky",
+        correcta: false
+      },
+      {
+        resp: "Ginebra",
+        correcta: false
+      },
+      {
+        resp: "Ron",
+        correcta: true
+      },
     ],
     img: "./imgs/tiki.jpg",
   },
   {
     pregunta: "¿Cual de estos ingredientes lleva el Long Island ice tea?",
-    respuestas: [
-      { resp: "Ginebra", correcta: false },
-      { resp: "Vodka", correcta: false },
-      { resp: "Tequila", correcta: false },
-      { resp: "Todas las opciones son correctas", correcta: true },
+    respuestas: [{
+        resp: "Ginebra",
+        correcta: false
+      },
+      {
+        resp: "Vodka",
+        correcta: false
+      },
+      {
+        resp: "Tequila",
+        correcta: false
+      },
+      {
+        resp: "Todas las opciones son correctas",
+        correcta: true
+      },
     ],
     img: "./imgs/long-island.jpg",
   },
   {
-    pregunta:
-      "¿Cual era la bebida favorita de las  chicas de Sexo en Nueva York?",
-    respuestas: [
-      { resp: "Bramble", correcta: false },
-      { resp: "Gin-tonic", correcta: false },
-      { resp: "Margarita", correcta: false },
-      { resp: "Cosmopolitan", correcta: true },
+    pregunta: "¿Cual era la bebida favorita de las  chicas de Sexo en Nueva York?",
+    respuestas: [{
+        resp: "Bramble",
+        correcta: false
+      },
+      {
+        resp: "Gin-tonic",
+        correcta: false
+      },
+      {
+        resp: "Margarita",
+        correcta: false
+      },
+      {
+        resp: "Cosmopolitan",
+        correcta: true
+      },
     ],
     img: "./imgs/sexoNY.jpg",
   },
   {
-    pregunta:
-      "¿Que coctelería inglesa se mantuvo durante 3 años consecutivos en el numero 1 de 50 world best bars?",
-    respuestas: [
-      { resp: "Nigthjar", correcta: false },
-      { resp: "Happines Forgets", correcta: false },
-      { resp: "Dandelyan", correcta: false },
-      { resp: "The Artesian", correcta: true },
+    pregunta: "¿Que coctelería inglesa se mantuvo durante 3 años consecutivos en el numero 1 de 50 world best bars?",
+    respuestas: [{
+        resp: "Nigthjar",
+        correcta: false
+      },
+      {
+        resp: "Happines Forgets",
+        correcta: false
+      },
+      {
+        resp: "Dandelyan",
+        correcta: false
+      },
+      {
+        resp: "The Artesian",
+        correcta: true
+      },
     ],
     img: "./imgs/Artesian1.jpg",
   },
   {
     pregunta: "¿Que tres ingredientes lleva un Margarita?",
-    respuestas: [
-      { resp: "Pisco, triple seco y zumo de lima", correcta: false },
-      { resp: "Tequila, ázucar y zumo de lima", correcta: false },
-      { resp: "Tequila, triple seco y ázucar", correcta: false },
-      { resp: "Tequila, triple seco y zumo de lima", correcta: true },
+    respuestas: [{
+        resp: "Pisco, triple seco y zumo de lima",
+        correcta: false
+      },
+      {
+        resp: "Tequila, ázucar y zumo de lima",
+        correcta: false
+      },
+      {
+        resp: "Tequila, triple seco y ázucar",
+        correcta: false
+      },
+      {
+        resp: "Tequila, triple seco y zumo de lima",
+        correcta: true
+      },
     ],
     img: "./imgs/margarita.jpg",
   },
   {
     pregunta: "¿Que se conoce como Speakeasy?",
-    respuestas: [
-      { resp: "Un licor", correcta: false },
-      { resp: "Un tipo de cerveza", correcta: false },
-      { resp: "Una bebida tradicional checa", correcta: false },
-      { resp: "Un bar oculto", correcta: true },
+    respuestas: [{
+        resp: "Un licor",
+        correcta: false
+      },
+      {
+        resp: "Un tipo de cerveza",
+        correcta: false
+      },
+      {
+        resp: "Una bebida tradicional checa",
+        correcta: false
+      },
+      {
+        resp: "Un bar oculto",
+        correcta: true
+      },
     ],
     img: "./imgs/speak.jpg",
   },
   {
     pregunta: "¿Que día acabó la ley seca en EEUU?",
-    respuestas: [
-      { resp: "4 de Junio 1935", correcta: false },
-      { resp: "22 de Enero 1928", correcta: false },
-      { resp: "7 de Noviembre 1932", correcta: false },
-      { resp: "5 de Diciembre de 1933", correcta: true },
+    respuestas: [{
+        resp: "4 de Junio 1935",
+        correcta: false
+      },
+      {
+        resp: "22 de Enero 1928",
+        correcta: false
+      },
+      {
+        resp: "7 de Noviembre 1932",
+        correcta: false
+      },
+      {
+        resp: "5 de Diciembre de 1933",
+        correcta: true
+      },
     ],
     img: "./imgs/leyseca.jpg",
   },
@@ -166,8 +313,10 @@ class Player {
   constructor(nombre) {
     this.nombre = nombre;
   }
-  getName () {
-    let nombre = document.getElementById("nombreUsuario").value
+  getName(nombre) {
+    this.nombre = document.getElementById("nombreUsuario").value
+    let nombreUsuario = document.getElementById("playerName")
+    nombreUsuario.innerText = this.nombre
   }
 }
 //Constructor de partida
@@ -178,26 +327,44 @@ class Partida {
     this.preguntaActual = this.preguntas[this.indice];
     this.score = 0;
   }
-  //Comprobar respuesta ganadora
+  //Comprobar respuesta ganadora  
 
   respuestaCorrecta(respuestaUsuario) {
     let respuestaCorrecta = null;
-    for (let i = 0; i < preguntas[this.indice].respuestas.length; i++) {
-      if (preguntas[this.indice].respuestas[i].correcta) {
-        respuestaCorrecta = preguntas[this.indice].respuestas[i].resp;
+    console.log(this.indice)
+    for (let i = 0; i < this.preguntas[this.indice].respuestas.length; i++) {
+      if (this.preguntas[this.indice].respuestas[i].correcta) {
+        respuestaCorrecta = this.preguntas[this.indice].respuestas[i].resp;
       }
     }
     if (respuestaUsuario == respuestaCorrecta) {
       return true;
     }
     return false;
+
   }
   //Actualizar Score
   actualizarScore(acierto) {
     if (acierto) {
-      score = score + 5;
+      this.score = this.score + 5;
+      document.getElementsByClassName("score1").innerHTML = this.score
     }
-   
+
+  }
+  //Timer
+  mostrarTimer() {
+    const tiempoPregunta = 10
+    const anchoBarra = 150
+    let contador = 0
+    const anchoColoreado = anchoBarra / tiempoPregunta
+    if (contador <= tiempoPregunta) {
+      cuenta.innerHTML = contador
+      timer.style.width = anchoColoreado * contador + "px"
+      contador++
+    } else {
+      // contador = 0!respuestaCorrecta()
+      siguientePregunta()
+    }
   }
   //Comprobar si ha acabado la partida al llegar a 10 rondas
   gameOver() {
@@ -224,8 +391,10 @@ class Partida {
     }
   }
 
+
+
   //Metodo para siguiente pregunta
-  siguientePregunta() {
+  mostrarPregunta() {
     //Mostrar 1 pregunta
     document.getElementById("pregunta").innerHTML = this.preguntas[
       this.indice
@@ -247,6 +416,9 @@ class Partida {
     //Mostrar score
     document.getElementsByClassName("score1").innerHTML = "Score = " + this.score;
 
+    //Timer
+    setInterval(this.mostrarTimer, 1000)
+
 
     //Reseteo color opciones respuesta
     const botones = document.getElementsByClassName("op");
@@ -254,8 +426,21 @@ class Partida {
       boton.style.backgroundColor = "#DDD";
     });
 
+
+  }
+  iniciarJuego() {
+    this.mostrarPregunta()
     //Icremento indice
-    this.indice++;
+    this.indice += 1
+    //Actualizar score
+
+    this.actualizarScore();
+    // Check game over
+    this.gameOver();
+  }
+  siguiente() {
+    this.mostrarPregunta()
+
     //Actualizar score
 
     this.actualizarScore();
@@ -276,7 +461,7 @@ class Board {
     this.opcionD = document.getElementById("d");
   }
 }
-let player1 = new Player(document.getElementById("nombreUsuario").value);
+let player = new Player();
 let board = new Board();
 let partida = new Partida();
 
@@ -292,11 +477,14 @@ document.getElementById("inicio").onclick = (evento) => {
   document.getElementById("siguiente").style.display = "inline";
   // boton siguiente pregunta
   document.getElementById("siguiente").onclick = (evento) => {
-    partida.siguientePregunta();
-  };
+    player.getName();
+    partida.siguiente();
 
+
+  };
+  player.getName()
   //inicio partida
-  partida.siguientePregunta();
+  partida.iniciarJuego();
 };
 // Colorear respuestas/Sonidos respuesta
 
@@ -307,7 +495,7 @@ let fail = document.getElementById("fail");
 Array.from(respuestasClick).forEach(function (elemento) {
   elemento.addEventListener("click", (event) => {
     if (partida.respuestaCorrecta(event.target.innerHTML)) {
-      event.style.backgroundColor = "green";
+      event.target.style.backgroundColor = "green";
       aplauso.play();
     } else {
       event.target.style.backgroundColor = "red";
