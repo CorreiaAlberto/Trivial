2a8a01e9f1eade599062c0f05001ca9bb6d11f13
CorreiaAlberto <alberto_cpv@hotmail.com>
2020-05-07 19:34:49 +0200

    commit final?

---
+++ javascript/index.js
@@ -0,0 +1,296 @@
+//Preguntas
+const preguntas = [{
+        pregunta: "¿Que cocktail popularizó la pelicula El gran Lebowsky?",
+        respuestas: [{
+                resp: "Cosmopolitan",
+                correcta: false
+            },
+            {
+                resp: "Old fashioned",
+                correcta: false
+            },
+            {
+                resp: "Mojito",
+                correcta: false
+            },
+            {
+                resp: "White russian",
+                correcta: true
+            },
+        ],
+        img: "./imgs/Ruso-blanco.jpg.",
+    },
+    {
+        pregunta: "¿Cual de los siguientes ingredientes NO lleva un bloody mary?",
+        respuestas: [{
+                resp: "Salsa Worcester",
+                correcta: false
+            },
+            {
+                resp: "Zumo de tomate",
+                correcta: false
+            },
+            {
+                resp: "Tabasco",
+                correcta: false
+            },
+            {
+                resp: "Ginebra",
+                correcta: true
+            },
+        ],
+        img: "./imgs/bloody mary.jpg",
+    },
+    {
+        pregunta: "¿Que cocktail hizo aparición durante la segunda parte de El padrino?",
+        respuestas: [{
+                resp: "Dry martini",
+                correcta: false
+            },
+            {
+                resp: "Tom Collins",
+                correcta: false
+            },
+            {
+                resp: "Negroni",
+                correcta: false
+            },
+            {
+                resp: "Banana daiquiri",
+                correcta: true
+            },
+        ],
+        img: "./imgs/el padrino.jpg",
+    },
+    {
+        pregunta: "¿Cual es el nombre del cocktail favorito de James Bond?",
+        respuestas: [{
+                resp: "Vodka Martini",
+                correcta: false
+            },
+            {
+                resp: "Coco loco",
+                correcta: false
+            },
+            {
+                resp: "Bondtini",
+                correcta: false
+            },
+            {
+                resp: "Vesper Martini",
+                correcta: true
+            },
+        ],
+        img: "./imgs/james bond.jpg",
+    },
+    {
+        pregunta: "¿Cual es el nombre del cocktail que vemos preparar en la imagen a Jerry Thomas? ",
+        respuestas: [{
+                resp: "Blue lagoon",
+                correcta: false
+            },
+            {
+                resp: "Manhattan",
+                correcta: false
+            },
+            {
+                resp: "Mint julep",
+                correcta: false
+            },
+            {
+                resp: "Blue blazer",
+                correcta: true
+            },
+        ],
+        img: "./imgs/Jerry-Thomas.jpg",
+    },
+    {
+        pregunta: "¿Que establecimiento cubano comercializó por primerar vez el popular mojito?",
+        respuestas: [{
+                resp: "La floridita",
+                correcta: false
+            },
+            {
+                resp: "El bribón de la havana",
+                correcta: false
+            },
+            {
+                resp: "La cantina del Malecón",
+                correcta: false
+            },
+            {
+                resp: "La bodedguita de en medio",
+                correcta: true
+            },
+        ],
+        img: "./imgs/mojito.jpg",
+    },
+    {
+        pregunta: "¿Cual es la bebida favorita de Don Draper, protagonista de Mad men?",
+        respuestas: [{
+                resp: "Negroni",
+                correcta: false
+            },
+            {
+                resp: "Long island ice tea",
+                correcta: false
+            },
+            {
+                resp: "Agua con gas",
+                correcta: false
+            },
+            {
+                resp: "Old fashioned",
+                correcta: true
+            },
+        ],
+        img: "./imgs/madmen.jpg",
+    },
+    {
+        pregunta: "¿Cual es la base alcoholica predominante en la coctelería Tiki?",
+        respuestas: [{
+                resp: "Vodka",
+                correcta: false
+            },
+            {
+                resp: "Whisky",
+                correcta: false
+            },
+            {
+                resp: "Ginebra",
+                correcta: false
+            },
+            {
+                resp: "Ron",
+                correcta: true
+            },
+        ],
+        img: "./imgs/tiki.jpg",
+    },
+    {
+        pregunta: "¿Cual de estos ingredientes lleva el Long Island ice tea?",
+        respuestas: [{
+                resp: "Ginebra",
+                correcta: false
+            },
+            {
+                resp: "Vodka",
+                correcta: false
+            },
+            {
+                resp: "Tequila",
+                correcta: false
+            },
+            {
+                resp: "Todas las opciones son correctas",
+                correcta: true
+            },
+        ],
+        img: "./imgs/long.jpg",
+    },
+    {
+        pregunta: "¿Cual era la bebida favorita de las  chicas de Sexo en Nueva York?",
+        respuestas: [{
+                resp: "Bramble",
+                correcta: false
+            },
+            {
+                resp: "Gin-tonic",
+                correcta: false
+            },
+            {
+                resp: "Margarita",
+                correcta: false
+            },
+            {
+                resp: "Cosmopolitan",
+                correcta: true
+            },
+        ],
+        img: "./imgs/sexoNY.jpg",
+    },
+    {
+        pregunta: "¿Que coctelería inglesa se mantuvo durante 3 años consecutivos en el numero 1 de 50 world best bars?",
+        respuestas: [{
+                resp: "Nigthjar",
+                correcta: false
+            },
+            {
+                resp: "Happines Forgets",
+                correcta: false
+            },
+            {
+                resp: "Dandelyan",
+                correcta: false
+            },
+            {
+                resp: "The Artesian",
+                correcta: true
+            },
+        ],
+        img: "./imgs/Artesian1.jpg",
+    },
+    {
+        pregunta: "¿Que tres ingredientes lleva un Margarita?",
+        respuestas: [{
+                resp: "Pisco, triple seco y zumo de lima",
+                correcta: false
+            },
+            {
+                resp: "Tequila, ázucar y zumo de lima",
+                correcta: false
+            },
+            {
+                resp: "Tequila, triple seco y ázucar",
+                correcta: false
+            },
+            {
+                resp: "Tequila, triple seco y zumo de lima",
+                correcta: true
+            },
+        ],
+        img: "./imgs/margarita.jpg",
+    },
+    {
+        pregunta: "¿Que se conoce como Speakeasy?",
+        respuestas: [{
+                resp: "Un licor",
+                correcta: false
+            },
+            {
+                resp: "Un tipo de cerveza",
+                correcta: false
+            },
+            {
+                resp: "Una bebida tradicional checa",
+                correcta: false
+            },
+            {
+                resp: "Un bar oculto",
+                correcta: true
+            },
+        ],
+        img: "./imgs/speak.jpg",
+    },
+    {
+        pregunta: "¿Que día acabó la ley seca en EEUU?",
+        respuestas: [{
+                resp: "4 de Junio 1935",
+                correcta: false
+            },
+            {
+                resp: "22 de Enero 1928",
+                correcta: false
+            },
+            {
+                resp: "7 de Noviembre 1932",
+                correcta: false
+            },
+            {
+                resp: "5 de Diciembre de 1933",
+                correcta: true
+            },
+        ],
+        img: "./imgs/leyseca.jpg",
+    },
+];
